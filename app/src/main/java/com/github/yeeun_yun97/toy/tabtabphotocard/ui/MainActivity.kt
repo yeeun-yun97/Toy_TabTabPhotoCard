ea06d89e91cf3a18a80a710f8bc734417fc147a4
yyn9704@gmail.com <yyn9704@gmail.com>
2022-07-17 23:56:36 +0900

    moved mainactivity package

--- app/src/main/java/com/github/yeeun_yun97/toy/tabtabphotocard/ui/MainActivity.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/tabtabphotocard/ui/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.github.yeeun_yun97.toy.tabtabphotocard
+package com.github.yeeun_yun97.toy.tabtabphotocard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.yeeun_yun97.toy.tabtabphotocard.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
